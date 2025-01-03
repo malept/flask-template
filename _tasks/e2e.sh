dbe48fba8a15045bd75b009809794a9c0f9240d8
Mark Lee <python+template@lazymalevolence.com>
2025-01-02 23:48:01 -0800

    feat: format files

--- _tasks/e2e.sh
+++ _tasks/e2e.sh
@@ -6,11 +6,28 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 TEMPLATE_DIR="$DIR"/..
 ANSWERS_FILE=.copier-answers.yml
 
+fail() {
+  local message="$1"
+
+  echo "$message" >&2
+  exit 1
+}
+
 tempRepo="$(mktemp -d "${TMPDIR:-/tmp}/flask-template.e2e.XXXXXX")"
 
-#trap 'rm -rf "${tempRepo}"' EXIT
+if [[ -z ${E2E_DEBUG:-} ]]; then
+  trap 'rm -rf "${tempRepo}"' EXIT
+fi
 cp "$DIR/$ANSWERS_FILE" "$tempRepo"
 echo "_src_path: \"$TEMPLATE_DIR\"" >>"$tempRepo/$ANSWERS_FILE"
 
+mise trust "$tempRepo/mise.toml"
+trap 'mise trust --untrust "$tempRepo/mise.toml"' EXIT
+
 echo "E2E: Running copier to destination '$tempRepo'"
 copier recopy --skip-answered --trust "$tempRepo"
+
+readmeLength="$(wc -c "$tempRepo"/README.md | awk '{print $1}')"
+if [[ "$readmeLength" != 0 ]]; then
+  fail "Expected empty README.md from 'uv init', got $readmeLength characters"
+fi
